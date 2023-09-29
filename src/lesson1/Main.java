810971254d5641b600951e2e3a0eb8dfd9e47443
Deadpool 1652 <iyanovskiy.08@gmail.com>
2023-09-29 17:15:05 +0100

    first commit

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -2,9 +2,8 @@ package lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Illia!");
-        }
-
+        double a = Math.random();
+        System.out.println(a);
     }
+
 }
